2e4725a7957b20078b76f98be15009d002f2dd01
lyouduan <lyouduan@gmail.com>
2024-07-14 15:33:06 +0800

    r

--- src/InstancingAndCullingApp.cpp
+++ src/InstancingAndCullingApp.cpp
@@ -226,7 +226,7 @@ void InstancingAndCullingApp::UpdateObjectCBs()
 				localSpaceFrustum.CreateFromMatrix(localSpaceFrustum, camera.GetProj());
 				localSpaceFrustum.Transform(localSpaceFrustum, viewToLocal);
 
-				if (localSpaceFrustum.Contains(e->Bounds) != DirectX::DISJOINT)
+				if (localSpaceFrustum.Contains(e->Bounds) != DirectX::DISJOINT || (mFrustumCullingEnabled == false))
 				{
 					InstanceData data;
 					XMMATRIX texTransform = XMLoadFloat4x4(&instanceData[i].texTransform);
